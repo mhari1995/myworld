90a9732e8e9d12c693ef85cdb8125dcfd743358b
mhari1995 <32972778+mhari1995@users.noreply.github.com>
2017-10-21 08:09:26 +0530

    Add files via upload

---
+++ LuckyNumber01.java
@@ -0,0 +1,20 @@
+public class LuckyNumber01 {
+    public static void main(String[] args) {
+        String date = "15-March-2016";
+        System.out.println(getLuckyNumber(date));
+    }
+
+    public static int getLuckyNumber(String date) {
+       String day[] = date.split("\\s",0);
+       
+       
+    }
+
+    public static int convertMMMtoMM(String mon) {
+     
+    }
+    
+    public static int getSumOfDigits(int num) {
+         //ADD YOUR CODE HERE
+    }
+}
